10876b91d2cf56149f02b11e7a8f9b7f0e59dbce
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-07-19 15:54:40 +0200

    Delete console.log in ApodService call

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -2,7 +2,5 @@ angular.module('app')
     .controller('MainController', function($scope, ApodService) {
       ApodService.getAll().then(function(res) {
         $scope.apod = res.data;
-      }, function(err) {
-        console.log("APOD API call did not work");
       });
     });
