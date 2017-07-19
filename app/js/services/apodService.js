820fa4615dd1629a43f86380dac565e5b58df2eb
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-07-19 15:51:14 +0200

    Change API key

--- app/js/services/apodService.js
+++ app/js/services/apodService.js
@@ -2,7 +2,7 @@ angular.module('app')
   .service('ApodService', function($http) {
     return {
       getAll: function() {
-        return $http.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+        return $http.get('https://api.nasa.gov/planetary/apod?api_key=MCBSaNcdLUpgdJ546qOqEcBjFhsOm3AO4UDXFRVh');
       }
     };
   });
